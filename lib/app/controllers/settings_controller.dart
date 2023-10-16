3a5f73eb56426023f45b3e2c09ec9e92fecf8533
Resam Taghipour <resam.t@gmail.com>
2023-10-17 02:50:13 +0330

    Settings Page and Controller

--- lib/app/controllers/settings_controller.dart
+++ lib/app/controllers/settings_controller.dart
@@ -1,12 +1,36 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter_file_dialog/flutter_file_dialog.dart';
+import 'package:get/get.dart';
+import 'package:language_learning_assistant_app/app/components/main_components/app_dialogs.dart';
+import 'package:language_learning_assistant_app/app/components/update_components/app_check_update.dart';
 import 'package:language_learning_assistant_app/core/app_core_functions.dart';
 import 'package:language_learning_assistant_app/core/elements/core_controller.dart';
+import 'package:language_learning_assistant_app/data/data_models/core_data_models/app_data/app_data.dart';
+import 'package:language_learning_assistant_app/data/data_models/core_data_models/app_settings_data/app_setting_data.dart';
+import 'package:language_learning_assistant_app/data/extensions/data_models_extensions/extension_settings.dart';
+import 'package:language_learning_assistant_app/data/extensions/data_types_extensions/extension_string.dart';
 import 'package:language_learning_assistant_app/data/info/app_page_details.dart';
+import 'package:language_learning_assistant_app/data/resources/app_enums.dart';
+import 'package:language_learning_assistant_app/data/resources/app_texts.dart';
+import 'package:language_learning_assistant_app/data/storage/app_local_storage.dart';
 
 class SettingsController extends CoreController {
 
+  Rx<AppSettingData> appSettings = const AppSettingData().obs;
+
+  Rx<bool> darkMode = false.obs;
+  Rx<AppLanguages> selectedLanguage = AppLanguages.english.obs;
+  Rx<AppCalendarTypes> selectedCalendar = AppCalendarTypes.georgian.obs;
+
+  Rx<String> updateAvailableVersion = AppTexts.generalNotAvailable.obs;
+
   @override
   void dataInit() {
-    // clearAppData();
+    appSettings = const AppSettingData().loadFromStorage.obs;
+    functionCheckUpdateAvailableVersion();
   }
 
   @override
@@ -15,13 +39,131 @@ class SettingsController extends CoreController {
   }
 
   @override
-  void onInitFunction() {}
+  void onInitFunction() {
+    fillData();
+  }
 
   @override
   void onReadyFunction() {}
 
   @override
   void onCloseFunction() {
+    saveSettings();
     saveAppData();
+  }
+
+  fillData() {
+    darkMode.value = appSettings.value.darkMode ?? false;
+    appDebugPrint('Fill Setting Data Function Applied Data');
+    appSettings.listen((data) => fillData());
+  }
+
+  functionDarkModeOnChange(bool value) {
+    darkMode.value = value;
+    appDebugPrint('DarkMode Changed to ${darkMode.value}');
+    refresh();
+  }
+
+  functionCheckUpdateAvailableVersion() async {
+    updateAvailableVersion.value = await AppCheckUpdate().checkVersion();
+    appDebugPrint('Checked Update Version: ${updateAvailableVersion.value}');
+  }
+
+  functionGoToUpdatePage() => goToPage(AppPageDetails.update);
+
+  functionBackup() {
+    function() async {
+      Get.back();
+      AppData appdata = AppLocalStorage.to.exportData();
+      var jsonData = jsonEncode(appdata);
+      Uint8List data = jsonData.toString().toUInt8List;
+      SaveFileDialogParams saveParams = SaveFileDialogParams(
+          data: data, fileName: AppTexts.settingBackupFilename);
+      String? filePath = await FlutterFileDialog.saveFile(params: saveParams);
+      appDebugPrint('** Backup File Saved **');
+      appDebugPrint('Filename: ${saveParams.fileName}');
+      appDebugPrint('Path: ${saveParams.sourceFilePath}');
+      appDebugPrint('File Path: $filePath');
+    }
+
+    AppDialogs.appAlertDialogWithOkCancel(
+        AppTexts.warning, AppTexts.areYouSureDataExport, function, true);
+  }
+
+  functionRestore() {
+    function() async {
+      Get.back();
+      OpenFileDialogParams openFileParams =
+      const OpenFileDialogParams(dialogType: OpenFileDialogType.document);
+      String? importFilePath = await FlutterFileDialog.pickFile(params: openFileParams);
+      appDebugPrint('** Backup File Selected **');
+      appDebugPrint('File Path: $importFilePath');
+
+      File importFile = File(importFilePath!);
+      String stringData = String.fromCharCodes(importFile.readAsBytesSync());
+      var jsonData = jsonDecode(stringData) as Map<String, dynamic>;
+      AppData appData = AppData.fromJson(jsonData);
+      clearAppData();
+      AppLocalStorage.to.importData(appData);
+      appDebugPrint('** Data Imported **');
+    }
+
+    AppDialogs.appAlertDialogWithOkCancel(
+        AppTexts.warning, AppTexts.areYouSureDataMayLost, function, true);
+  }
+
+  functionClearDictionary() {
+    function() {
+      AppLocalStorage.to.clearSpecificKey(AppStorageKeys.keyDictionary);
+      Get.back();
+      appDebugPrint('Dictionary Items Cleared');
+    }
+
+    AppDialogs.appAlertDialogWithOkCancel(
+        AppTexts.warning, AppTexts.areYouSureDataWillLost, function, true);
+  }
+
+  functionClearVerbs() {
+    function() {
+      AppLocalStorage.to.clearSpecificKey(AppStorageKeys.keyVerbs);
+      Get.back();
+      appDebugPrint('Verbs Items Cleared');
+    }
+
+    AppDialogs.appAlertDialogWithOkCancel(
+        AppTexts.warning, AppTexts.areYouSureDataWillLost, function, true);
+  }
+
+  functionClearSaved() {
+    function() {
+      AppLocalStorage.to.clearSpecificKey(AppStorageKeys.keySaved);
+      Get.back();
+      appDebugPrint('Saved Items Cleared');
+    }
+
+    AppDialogs.appAlertDialogWithOkCancel(
+        AppTexts.warning, AppTexts.areYouSureDataWillLost, function, true);
+  }
+
+  functionClearAllData() {
+    function() {
+      clearAppData();
+      Get.back();
+      appDebugPrint('');
+    }
+
+    AppDialogs.appAlertDialogWithOkCancel(
+        AppTexts.warning, AppTexts.areYouSureDataWillLost, function, true);
+  }
+
+  saveSettings() {
+    appSettings.saveOnStorage;
+    appDebugPrint('Settings Saved');
+  }
+
+  resetAllSettings() {
+    appSettings.value = const AppSettingData().clearData;
+    saveSettings();
+    appDebugPrint('Reset Settings Data performed');
   }
 }
--- lib/app/views/settings_view.dart
+++ lib/app/views/settings_view.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:language_learning_assistant_app/app/components/general_components/app_popup_menu.dart';
+import 'package:language_learning_assistant_app/app/components/general_components/app_popup_menu_item.dart';
+import 'package:language_learning_assistant_app/app/components/general_components/app_switch.dart';
 import 'package:language_learning_assistant_app/app/components/main_components/app_bar.dart';
 import 'package:language_learning_assistant_app/app/components/main_components/app_bottom_navigation_bar.dart';
 import 'package:language_learning_assistant_app/app/components/main_components/app_drawer.dart';
+import 'package:language_learning_assistant_app/app/components/settings_components/settings_components.dart';
 import 'package:language_learning_assistant_app/app/controllers/settings_controller.dart';
 import 'package:language_learning_assistant_app/core/elements/core_view.dart';
+import 'package:language_learning_assistant_app/data/info/app_info.dart';
+import 'package:language_learning_assistant_app/data/resources/app_texts.dart';
 
 class SettingsPage extends CoreView<SettingsController> {
   const SettingsPage({Key? key}) : super(key: key);
@@ -29,5 +36,110 @@ class SettingsPage extends CoreView<SettingsController> {
   Widget? get floatingActionButton => null;
 
   @override
-  Widget get body => Column();
+  Widget get body => Column(children: [
+        widgetGeneral(),
+        widgetUpdate(),
+        widgetBackup(),
+        widgetStorage(),
+      ]);
+
+  Widget widgetAppbarThreeDotsButton() =>
+      AppPopupMenu(listItems: listAppbarThreeDotsButton, lightIcon: true);
+
+  List<AppPopupMenuItem> get listAppbarThreeDotsButton => List.of([
+        AppPopupMenuItem(
+            text: AppTexts.settingAppbarMenuResetSettings, onTapFunction: () {})
+      ]);
+
+  Widget widgetGeneral() {
+    ///TODO: Languages Implementation
+    Widget widgetLanguages() =>
+        Text(controller.selectedLanguage.value.name.capitalizeFirst ?? '');
+
+    ///TODO: Calendar Types Implementation
+    Widget widgetCalendar() =>
+        Text(controller.selectedCalendar.value.name.capitalizeFirst ?? '');
+
+    Widget widgetDarkMode() => Obx(() => AppSwitch(
+        value: controller.darkMode.value,
+        onChanged: (value) => controller.functionDarkModeOnChange(value),
+        enabled: false));
+
+    return SettingsComponents.widgetSettingSection(
+        SettingsComponents.widgetSectionTitle(
+            AppTexts.settingSectionTitleGeneral),
+        [
+          SettingsComponents.widgetSettingItem(
+              AppTexts.settingSectionTitleGeneralLanguage,
+              widgetLanguages(),
+              null),
+          SettingsComponents.widgetSettingItem(
+              AppTexts.settingSectionTitleGeneralCalendar,
+              widgetCalendar(),
+              null),
+          SettingsComponents.widgetSettingItem(
+              AppTexts.settingSectionGeneralItemDarkMode,
+              widgetDarkMode(),
+              null),
+        ]);
+  }
+
+  Widget widgetUpdate() {
+    return SettingsComponents.widgetSettingSection(
+        SettingsComponents.widgetSectionTitle(
+            AppTexts.settingSectionTitleUpdate),
+        [
+          SettingsComponents.widgetSettingItem(
+              AppTexts.settingSectionTitleUpdateCurrentVersion,
+              Text(AppInfo.appCurrentVersion),
+              null),
+          SettingsComponents.widgetSettingItem(
+              AppTexts.settingSectionTitleUpdateAvailableVersion,
+              Text(controller.updateAvailableVersion.value ==
+                      AppInfo.appCurrentVersion
+                  ? AppTexts.generalNotAvailable
+                  : controller.updateAvailableVersion.value),
+              controller.functionGoToUpdatePage),
+        ]);
+  }
+
+  Widget widgetBackup() {
+    return SettingsComponents.widgetSettingSection(
+        SettingsComponents.widgetSectionTitle(
+            AppTexts.settingSectionTitleBackup),
+        [
+          SettingsComponents.widgetSettingItem(
+              AppTexts.settingSectionBackupBackup,
+              null,
+              controller.functionBackup),
+          SettingsComponents.widgetSettingItem(
+              AppTexts.settingSectionBackupRestore,
+              null,
+              controller.functionRestore),
+        ]);
+  }
+
+  Widget widgetStorage() {
+    return SettingsComponents.widgetSettingSection(
+        SettingsComponents.widgetSectionTitle(
+            AppTexts.settingSectionTitleStorage),
+        [
+          SettingsComponents.widgetSettingItem(
+              AppTexts.settingSectionStorageItemEraseDictionary,
+              null,
+              controller.functionClearDictionary),
+          SettingsComponents.widgetSettingItem(
+              AppTexts.settingSectionStorageItemEraseVerbs,
+              null,
+              controller.functionClearVerbs),
+          SettingsComponents.widgetSettingItem(
+              AppTexts.settingSectionStorageItemEraseSaved,
+              null,
+              controller.functionClearSaved),
+          SettingsComponents.widgetSettingItem(
+              AppTexts.settingSectionStorageItemEraseAllData,
+              null,
+              controller.functionClearAllData),
+        ]);
+  }
 }
