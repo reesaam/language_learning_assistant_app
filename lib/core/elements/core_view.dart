766bba003770e7ca26e1bdefaad97aa3fb7dc00d
Resam Taghipour <resam.t@gmail.com>
2023-10-16 19:12:42 +0330

    CoreView and CoreController

---
+++ lib/core/elements/core_view.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:language_learning_assistant_app/core/app_core_functions.dart';
+import 'package:language_learning_assistant_app/core/app_core_widgets.dart';
+import 'package:language_learning_assistant_app/core/elements/core_controller.dart';
+import 'package:language_learning_assistant_app/data/resources/app_colors.dart';
+import 'package:language_learning_assistant_app/data/resources/app_paddings.dart';
+
+abstract class CoreView<Controller extends CoreController>
+    extends GetView<Controller> {
+  const CoreView({final Key? key}) : super(key: key);
+
+  Future<bool> onWillPop() async => onBackButtonPressed(controller.pageDetail);
+
+  ///Main Widgets
+  PreferredSizeWidget? get appBar;
+  Widget get body;
+  Widget? get topBar;
+  Widget? get footer;
+  Widget? get bottomNavigationBar;
+  Widget? get floatingActionButton;
+
+  @override
+  Widget build(BuildContext context) =>
+      WillPopScope(onWillPop: () => onWillPop(), child: _pageScaffold);
+
+  Widget get _pageScaffold => Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: appBar,
+        body: _pageBody,
+        bottomNavigationBar: bottomNavigationBar,
+        floatingActionButton: floatingActionButton,
+        backgroundColor: AppColors.appBackground,
+      );
+
+  Widget get _pageBody => SafeArea(
+        child: Column(children: [
+          topBar == null ? shrinkSizedBox : topBar!,
+          //Main Body
+          Expanded(
+              child: Padding(
+                  padding: AppPaddings.pages,
+                  child: SingleChildScrollView(
+                      scrollDirection: Axis.vertical,
+                      physics: const BouncingScrollPhysics(),
+                      child: body))),
+          footer == null ? shrinkSizedBox : footer!,
+        ]),
+      );
+}
